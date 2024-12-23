f29e949734b1dca214b6edec97d2b9dc2255650d
shinwoos <kmnj2100@gmail.com>
2024-12-24 00:15:38 +0900

    step 10

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,11 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
+import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
@@ -24,6 +28,10 @@ class App {
         System.out.println("== 명언 앱 ==");
 
         int cnt = 1; // 몇 번 명언인 지 구분하는 카운트 값
+
+        JSONArray jsonArray = new JSONArray(); // 명령: 빌드를 위한 json array 참조 변수
+//        JSONParser parser = new JSONParser();
+
         while (true) {
             String cmd;
             String wise;
@@ -59,7 +67,18 @@ class App {
                 ObjectNode obj = createjsonObj(cnt,wise,author);
 
                 try{
+                    // lastid 생성
+                    String fileName = "D:/dev/dev_back4/wise-saying/db/wiseSaying/lastId.txt";
+                    BufferedWriter fw = new BufferedWriter((new FileWriter(fileName)));
+
+                    fw.write(Integer.toString(cnt));
+                    fw.flush();
+                    fw.close();
+
+                    // json 생성 구문
                     String beautyJson = beautyJson(obj);
+
+                    jsonArray.add(obj);
 
                     FileWriter file = new FileWriter("D:/dev/dev_back4/wise-saying/db/wiseSaying/"+cnt+".json");
                     file.write(beautyJson);
@@ -88,6 +107,18 @@ class App {
                 if(!wiseMap.containsKey(Integer.parseInt(removeKey))){
                     System.out.println(removeKey+"번 명언은 존재하지 않습니다.");
                 }else{
+
+                    for (int i = 0; i < jsonArray.size(); i++) {
+                        ObjectNode currentObj = (ObjectNode) jsonArray.get(i);
+
+                        // 특정 조건에 맞는 데이터 삭제
+                        if (removeKey.equals(currentObj.get("id").asText())) {
+                            jsonArray.remove(i);  // i번째 인덱스 삭제
+                            break;
+                        }
+                    }
+
+
                     wiseMap.remove(Integer.parseInt(removeKey));
                     System.out.println(removeKey + "번 명언이 삭제되었습니다.");
                 };
@@ -118,6 +149,21 @@ class App {
                     try{
                         String beautyJson = beautyJson(obj);
 
+                        for (int i = 0; i < jsonArray.size(); i++) {
+                            ObjectNode currentObj = (ObjectNode) jsonArray.get(i);
+
+                            // 특정 조건에 맞는 데이터 수정
+                            if (updateKey.equals(currentObj.get("id").asText())) {
+                                // 수정할 데이터 생성
+                                ObjectNode updatedObj = currentObj.deepCopy();
+                                updatedObj.put("content",wise+" "+updateKey);
+                                updatedObj.put("author",author+" "+updateKey);
+
+                                // 같은 인덱스에 수정된 데이터 설정
+                                jsonArray.set(i, updatedObj);
+                            }
+                        }
+
                         FileWriter file = new FileWriter("D:/dev/dev_back4/wise-saying/db/wiseSaying/"+updateKey+".json");
                         file.write(beautyJson);
                         file.flush();
@@ -127,6 +173,22 @@ class App {
                         e.printStackTrace();
                     }
                 }
+            }else if(cmd.equals("빌드")){
+
+                System.out.println(jsonArray);
+
+                try{
+                    String beautyJson = beautyJson(jsonArray);
+
+                    FileWriter file = new FileWriter("D:/dev/dev_back4/wise-saying/db/wiseSaying/data.json");
+                    file.write(beautyJson);
+                    file.flush();
+                    file.close();
+
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+
             }
         }
     }
@@ -140,8 +202,6 @@ class App {
         obj.put("id",cnt);
         obj.put("content",wise+" "+cnt);
         obj.put("author",author+" "+cnt);
-
-        System.out.println(obj.toString());
 
         return obj;
 
@@ -157,4 +217,15 @@ class App {
             return beautyJson;
 
     }
+
+    public String beautyJson(JSONArray obj) throws IOException{
+
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            Object json = objectMapper.readValue(obj.toString(), Object.class);
+            String beautyJson = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(json);
+            return beautyJson;
+
+    }
+
 }
