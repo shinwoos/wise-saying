e626190fba925876883fe4d34e767e18591e526f
shinwoos <kmnj2100@gmail.com>
2024-12-23 15:49:11 +0900

    up cnt wise

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,6 +14,7 @@ class App {
 
         System.out.println("== 명언 앱 ==");
 
+        int cnt = 1; // 몇 번 명언인 지 구분하는 카운트 값
         while (true) {
             String cmd;
 
@@ -21,17 +22,20 @@ class App {
             cmd = scanner.nextLine(); // 입력값 가져옴 / 입력값이 없으면 기다린다.
 
             if (cmd.equals("등록")) {
+
                 System.out.print("명언 : ");
                 scanner.nextLine(); // 입력값 가져옴 / 입력값이 없으면 기다린다.
 
                 System.out.print("작가 : ");
                 scanner.nextLine();
 
-                System.out.println("1번 명언이 등록되었습니다.");
+                System.out.println(cnt+"번 명언이 등록되었습니다.");
+                cnt++;
+
+            }else if(cmd.equals("종료")){
+                System.out.println("명언 앱을 종료합니다.");
+                break;
             }
-//            System.out.println("명령) 종료");
         }
-
-
     }
 }
